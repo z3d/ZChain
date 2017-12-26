3f0b38d2395fe809610d92e0b6bb2a3be156f6a4
z3d <925699+z3d@users.noreply.github.com>
2017-12-26 23:51:45 +1000

    Change transaction numbers

--- src/ZChain/Program.cs
+++ src/ZChain/Program.cs
@@ -7,22 +7,22 @@ namespace ZChain
     {
         static void Main(string[] args)
         {
-            var genesisBlock = Block.CreateGenesisBlock(new Transaction("First_Address", "Second_Address", 300), 10);
+            var genesisBlock = Block.CreateGenesisBlock(new Transaction("First_Address", "Second_Address", 300), 5);
             genesisBlock.MineBlock();
             Console.WriteLine(genesisBlock);
             Console.WriteLine($"Verified: {Block.VerifyHash(genesisBlock)}");
 
 
-            var secondBlock = new Block(genesisBlock, new Transaction("Second_Address", "Third_Address", 400), 10);
+            var secondBlock = new Block(genesisBlock, new Transaction("Second_Address", "Third_Address", 200), 5);
             secondBlock.MineBlock();
-
             Console.WriteLine(secondBlock);
             Console.WriteLine($"Verified: {Block.VerifyHash(secondBlock)}");
 
-            var thirdBlock = new Block(secondBlock, new Transaction("ThirdAddress", "FourthAddress", 400), 10);
+            var thirdBlock = new Block(secondBlock, new Transaction("ThirdAddress", "FourthAddress", 100), 5);
             thirdBlock.MineBlock();
             Console.WriteLine(thirdBlock);
             Console.WriteLine($"Verified: {Block.VerifyHash(thirdBlock)}");
+
             Console.ReadLine();
         }
     }
