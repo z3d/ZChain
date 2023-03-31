73b4198aa1b4fc7f4714d11bfb0f854ac0c5ecca
z3d <925699+z3d@users.noreply.github.com>
2023-03-31 15:45:44 +1000

    Fix unit tests

--- src/ZChain.Tests/UnitTests/Domain/BlockTests/BlockTests.cs
+++ src/ZChain.Tests/UnitTests/Domain/BlockTests/BlockTests.cs
@@ -8,8 +8,9 @@ namespace ZChain.Tests.UnitTests.Domain.BlockTests
 {
     public class BlockTests
     {
-        private readonly Block<MoneyTransferDummyTransaction> _firstChild;
+        private readonly Block<MoneyTransferDummyTransaction> _rootBlock = new Block<MoneyTransferDummyTransaction>(null, new MoneyTransferDummyTransaction("First_Address", "Second_Address", 300), 4);
         private readonly MoneyTransferDummyTransaction _moneyTransferDummyTransaction = new MoneyTransferDummyTransaction("Second_Address", "Third_Address", 200);
+        private readonly IMiner<MoneyTransferDummyTransaction> _miner = new StubMiner<MoneyTransferDummyTransaction>("c349c83bd38c46c48321c7f9fbcffe3a", "0000BF5EBA588845258F54C0E3D07EE2CCA2E3F01135225D291BF0717353AA67"); 
 
         class StubMiner<T> : IMiner<T>
         {
@@ -31,35 +32,27 @@ namespace ZChain.Tests.UnitTests.Domain.BlockTests
         }
 
         public BlockTests()
-        {
-            var rootBlock = new Block<MoneyTransferDummyTransaction>(null, new MoneyTransferDummyTransaction("First_Address", "Second_Address",300), 4);
-            var miner = new StubMiner<MoneyTransferDummyTransaction>("d31d42f5137b4f32ae8b78875d8e46e1", "0000E4898D26DB1BAF66E6AD187F5573117946C14F4021046461DCF0EDADE1EC");
-            miner.MineBlock(rootBlock);
-            _firstChild = new Block<MoneyTransferDummyTransaction>(rootBlock, _moneyTransferDummyTransaction, 4);
+        {           
+            _miner.MineBlock(_rootBlock);            
         }
         
         [Fact]
         public void WhenHavingAMinedBlock_AndAttemptingToRemine_ShouldThrow()
         {
-            var miner = new StubMiner<MoneyTransferDummyTransaction>("a80a69ff16b94b3784bb200db2638b91", "0000FFF686BF9624BACCF8CB9E6F02B96450FF8C844A043C4072719D007833C4");
-            miner.MineBlock(_firstChild);
-
-            Should.Throw<InvalidOperationException>(() => miner.MineBlock(_firstChild));
+           Should.Throw<InvalidOperationException>(() => _miner.MineBlock(_rootBlock));
         }
 
         [Fact]
         public void WhenMiningABlock_AndSettingABadNonce_ShouldThrow()
-        {
-            var miner = new StubMiner<MoneyTransferDummyTransaction>("31e359e12e9a4ad19559fbffe8f3c15a", "0041AD4CC3B9C508336BA949D302076E0A47DAB769B7D2A6F216791A913A4036");
-            Should.Throw<BlockStateException>(() => miner.MineBlock(_firstChild));
+        {            
+            var firstChild = new Block<MoneyTransferDummyTransaction>(_rootBlock, _moneyTransferDummyTransaction, 4);
+            Should.Throw<BlockStateException>(() => _miner.MineBlock(firstChild));
         }
 
         [Fact]
         public void WhenHavingANewBlock_AndAttemptingToVerify_ShouldThrow()
-        {
-            var rootBlock = new Block<MoneyTransferDummyTransaction>(null, _moneyTransferDummyTransaction, 2);
-
-            var newBlock = new Block<MoneyTransferDummyTransaction>(rootBlock, _moneyTransferDummyTransaction, 2);
+        { 
+            var newBlock = new Block<MoneyTransferDummyTransaction>(_rootBlock, _moneyTransferDummyTransaction, 2);
 
             Should.Throw<BlockStateException>(() => newBlock.VerifyMinedBlock());
         }
@@ -69,9 +62,7 @@ namespace ZChain.Tests.UnitTests.Domain.BlockTests
         {
             var rootBlock = new Block<MoneyTransferDummyTransaction>(null, _moneyTransferDummyTransaction, 2);
             var newBlock = new Block<MoneyTransferDummyTransaction>(rootBlock, _moneyTransferDummyTransaction, 2);
-
-            var miner = new StubMiner<MoneyTransferDummyTransaction>("5b890bf59d1b421aa7e02b6fba2524be", "05EEC80E461F2908F7A6070990699F233383539798E5BAD3F94DBD9096554F5D");
-            Should.Throw<BlockStateException>(() => miner.MineBlock(newBlock));
+            Should.Throw<BlockStateException>(() => _miner.MineBlock(newBlock));
         }
     }
 }
